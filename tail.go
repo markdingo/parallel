aa2c1514dcdf8e8c822fe4dfdfb567300017e7ae
Mark Delany <j6z@golf.emu.st>
2023-10-08 13:18:16 +1000

    Tidy tail.go

--- tail.go
+++ tail.go
@@ -11,7 +11,7 @@ import (
 // has a "next" writer so getting, setting and closing functions are all no-ops.
 //
 // Most importantly, tail protects the Group output writers from concurrent access by all
-// runners within the Group.
+// runners within the Group via a group-wide mutex.
 type tail struct {
 	out      io.Writer
 	outputMu *sync.Mutex
@@ -21,17 +21,12 @@ func newTail(out io.Writer, outputMu *sync.Mutex) *tail {
 	return &tail{out: out, outputMu: outputMu}
 }
 
-func (wtr *tail) getNext() writer {
-	return nil
-}
-
-func (wtr *tail) setNext(out writer) {
-}
+func (wtr *tail) getNext() writer { return nil }
+func (wtr *tail) setNext(writer)  {}
+func (wtr *tail) close()          {}
 
 func (wtr *tail) Write(p []byte) (n int, err error) {
 	wtr.outputMu.Lock()
 	defer wtr.outputMu.Unlock()
 	return wtr.out.Write(p)
 }
-
-func (wtr *tail) close() {}
